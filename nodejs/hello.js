033d821ec13e0530ffaeba5b4227f3f7d06b842d
kihwn <kihwn98@gmail.com>
2024-05-15 18:41:45 +0900

    nodejs 성적 파일은 이후 수정 예정

---
+++ nodejs/hello.js
@@ -0,0 +1,9 @@
+//파일명: hello.js
+let a = 10;
+let b = 20;
+
+console.log(`a=%{a} b=${b} result=${a+b}`);
+//실행은 F5를 누르고 Node.js를 누른다.
+//ctrl 누르고 terminal탭을 보면 pwoershell(ps)가 있음.
+//여기에서 'node파일명' 안되면
+//powershell 옆에 밑으로 화살표에 command prompt
