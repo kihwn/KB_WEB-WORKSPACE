bde3c1e3230e73027b96994cfee48cca3d8e1344
kihwn <cristine980@konkuk.ac.kr>
2024-05-27 22:08:52 +0900

    vue_component 과제

---
+++ webpack-app/vue-project4/src/components/ImageBox.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="app">
+        <div class="image-container">
+            <img src="./assets/my_image.jpg" alt="My Image" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
