ba8b834f24e4250a57b6bc953e1a764545483f94
kihwn <cristine980@konkuk.ac.kr>
2024-05-27 15:37:59 +0900

    vue_component 활용
    vscode 내에서 파일 생성 및 활용 부분에서 권한 문제 발생. 터미널 창에서 sudo chmod를 활용하여 해당 파일에 대한 권한을 줌

--- vue_workspace/myapp2/src/App.vue
+++ vue_workspace/myapp2/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyCounter/>
+  <MyCalculator/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import MyCounter from './components/MyCounter.vue'
+import MyCalculator from './components/MyCalculator.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+    MyCounter,
+    MyCalculator
   }
 }
 </script>
---
+++ vue_workspace/myapp2/src/components/MyCalculator.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div>
+        <h1>계산기</h1>
+        x: <input type="text" v-model="x"><br/>
+        y: <input type="text" v-model="y"><br/>
+        result: <input type="text" v-model="result"><br/>
+        <button type="button" v-on:click="add">+</button>
+        <button type="button" v-on:click="sub">-</button>
+        <button type="button" v-on:click="mul">*</button>
+        <button type="button" v-on:click="div">/</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            x:0,
+            y:0,
+            result:0
+        }
+    },
+    methods:{
+        add:function(){this.result = parseInt(this.x)+parseInt(this.y);},
+        sub:function(){this.result = parseInt(this.x)-parseInt(this.y);},
+        mul:function(){this.result = parseInt(this.x)*parseInt(this.y);},
+        div:function(){this.result = parseInt(this.x)/parseInt(this.y);}
+    }
+}
+</script>
+<style lang="">
+</style>
