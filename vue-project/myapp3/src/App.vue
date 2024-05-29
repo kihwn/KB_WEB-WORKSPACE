02b2709d16ef86a4eaf0c3bb7282fe4cda5fc929
kihwn <cristine980@konkuk.ac.kr>
2024-05-29 16:25:09 +0900

    vue_API를 받아와서 데이터를 화면에 출력하는 예제

--- vue-project/myapp3/src/App.vue
+++ vue-project/myapp3/src/App.vue
@@ -1,12 +1,12 @@
-
-
 <script setup>
-import HeroMain from "./components/hero/HeroMain.vue";
+//import HeroMain from "./components/hero/HeroMain.vue";
+import TodoList from "./components/todos/TodoList.vue";
 </script>
 <template>
   <h1>Hero</h1>
   <hr>
-  <HeroMain></HeroMain>
+  <!--<HeroMain></HeroMain>-->
+  <TodoList></TodoList>
 </template>
 <style scoped>
 </style>
---
+++ vue-project/myapp3/src/components/todos/TodoList.vue
@@ -0,0 +1,40 @@
+<template lang="">
+    <div>
+        <button type="button" v-on:click="loadData">데이터불러오기</button> 
+        <br/>
+        <ul>
+            <li v-for="todo in todoList">
+            {{todo.id}}
+            {{todo.userId}}
+            {{todo.title}}
+            {{todo.completed}}
+        </li>
+        </ul>
+        
+    </div>
+</template>
+<script>
+export default {
+    //이 부분이 감시되면서 지속적인 렌더링 작업이 벌어진다. 
+    data(){
+        return{
+            todoList:[]
+        }
+    },
+    
+    methods:{
+        loadData:function(){
+            fetch('http://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(data =>{
+                this.todoList = data;
+            })
+
+        }
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
